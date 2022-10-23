f82b39912ae2cbd52e99293ee12ab9b4bad83093
Amit Ferman <amitferman@gmail.com>
2022-10-23 10:52:59 -0700

    Logo Font

--- components/Logo/index.js
+++ components/Logo/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function Logo() {
   return (
    <Link href="/">
-     <a className="font-bold hover:underline">Vantage</a>
+     <a className="font-bold hover:underline el-mesiri-font">Vantage</a>
    </Link>
   );
 }
